94223778bf45c2e389159ab2a740a8f3b2115197
Stefano Lottini <stefano.lottini@datastax.com>
2022-08-13 00:41:08 +0200

    start_jupyter.sh

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # A test of Gitpod and Jupyter being BFF
 
+[Click here, please](gitpod.io/#https://github.com/hemidactylus/gitpod-jupyter)
 
---
+++ start_jupyter.sh
@@ -0,0 +1,30 @@
+#~/bin/bash
+
+jupyter notebook \
+    --no-browser \
+    --NotebookApp.allow_origin=\'$(gp url 8888)\' \
+    2>&1 \
+  | tee .jupyter_log.log &
+
+echo "CHECKING";
+J_TOKEN="";
+for WAIT in `seq 1 30`; do
+  echo "TOKEN-TESTING ${WAIT}";
+  J_TOKEN=`grep token .jupyter_log.log | awk -F= '{print $2}' | head -n 1`
+  if [ -n "${J_TOKEN}" ]; then
+    echo "FOUND/LEAVING";
+    break;
+  fi
+  echo "SLEEPING";
+  sleep 0.5;
+done
+
+if [ -n "${J_TOKEN}" ]; then
+  echo "TOKEN WAS FOUND";
+  JUPYTER_URL="$(gp url 8888)/?token=${J_TOKEN}"
+  echo -e "\n\n\n\n\n\t\t** OPENING NOTEBOOK IN NEW TAB. PLEASE CHECK YOUR POP-UP BLOCKER **\n";
+  gp preview --external "${JUPYTER_URL}";
+else
+  echo "Could not start Jupyter successfully.";
+  exit 1;
+fi
